0dcd3cf861f694acf6dfc543ffd9326115521605
saiteja <saitejalimbagiri3910@gmail.com>
2020-11-20 16:50:02 +0530

    minor bugs

--- peerxp/settings.py
+++ peerxp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^g+=r1u6w(#f*t8up=@tlz)oym)b-c7(8#78zbz@h7@jkui1!@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
